91f49f44fab7cdf7645264e8c5bc0a1524c79bb0
Andrew Henry <andrewmhenry@me.com>
2022-06-27 16:59:10 +1000

    🚀 feat(Bundlr Config): install and create a bundlr instance, removed Rainbowkit for now as won't be using it

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,39 @@
+import { useState, useRef } from 'react'
 import { ThemeProvider } from 'theme-ui'
+import { WebBundlr } from '@bundlr-network/client'
+import { providers } from 'ethers'
 import Theme from '../theme'
 import Layout from '../components/layout'
 import '../styles.css'
-import '@rainbow-me/rainbowkit/styles.css'
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
-
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
-)
-const { connectors } = getDefaultWallets({
-  appName: 'My RainbowKit App',
-  chains,
-})
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-})
 
 function App({ Component, pageProps }) {
+  const [bundlrInstance, setBundlrInstance] = useState()
+  const [balance, setBalance] = useState(0)
+
+  // set the base currency as matic (this can be changed later in the app)
+  const [currency, setCurrency] = useState('matic')
+  const bundlrRef = useRef()
+
+  // create a function to connect to bundlr network
+  async function initialiseBundlr() {
+    await window.ethereum.enable()
+
+    const provider = new providers.Web3Provider(window.ethereum)
+    await provider._ready()
+
+    const bundlr = new WebBundlr('https://node1.bundlr.network', currency, provider)
+    await bundlr.ready()
+
+    setBundlrInstance(bundlr)
+    bundlrRef.current = bundlr
+    // fetchBalance()
+  }
+
   return (
     <ThemeProvider theme={Theme}>
       <Layout>
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider chains={chains}>
-            <Component {...pageProps} />
-          </RainbowKitProvider>
-        </WagmiConfig>
+        <button onClick={initialiseBundlr}>Connect Bundlr</button>
+        <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 /** @jsxImportSource theme-ui */
-import { ConnectButton } from '@rainbow-me/rainbowkit'
 import ContainerPage from '../components/container-page'
 
 export default function Home() {
   return (
     <ContainerPage>
       <h3 sx={{ color: 'primary' }}>Home Page</h3>
-      <ConnectButton />
     </ContainerPage>
   )
 }
