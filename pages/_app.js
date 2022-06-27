44cef2db9d73d4080a344d767114810aa47d2d84
Andrew Henry <andrewmhenry@me.com>
2022-06-27 12:06:19 +1000

    🚀 feat(Wallet Connection): installed and implemnted RainbowKit for users to connect their wallet

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,35 @@ import { ThemeProvider } from 'theme-ui'
 import Theme from '../theme'
 import Layout from '../components/layout'
 import '../styles.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+)
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+})
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
 
 function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={Theme}>
       <Layout>
-        <Component {...pageProps} />
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
       </Layout>
     </ThemeProvider>
   )
